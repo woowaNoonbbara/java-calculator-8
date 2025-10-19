64dd362d34667c8e80d3d62e1f88ed27180612d2
noonbbara <ysys1226@naver.com>
2025-10-20 01:21:14 +0900

    chore(application): 주석 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -15,7 +15,7 @@ public class Application {
 
     public static int calculate(String input) {
         if (input == null || input.isEmpty()) throw new IllegalArgumentException();
-        input = input.replace("\\n", "\n");
+        input = input.replace("\\n", "\n"); // test : 커스텀_구분자_사용() 통과를 위해 추가
 
         String delimiter;
         int sum = 0;
