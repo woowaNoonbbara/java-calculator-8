15425abb55a252d03ea84a5bbfbedd3dc7b6061e
noonbbara <ysys1226@naver.com>
2025-10-19 23:57:24 +0900

    feat(Application): 쉼표, 콜론 구분자로 계산기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,32 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = Console.readLine();
+        int result = calculate(input);
+
+        System.out.println(result);
+    }
+
+    public static int calculate(String input) {
+        if (input == null || input.isEmpty()) throw new IllegalArgumentException();
+
+        String delimiter = ",|:";
+
+        String[] numbers = input.split(delimiter);
+        int sum = 0;
+        for (String n : numbers) {
+            int num = Integer.parseInt(n);
+            if (num < 0) throw new IllegalArgumentException();
+            sum += num;
+        }
+
+        return sum;
     }
 }
