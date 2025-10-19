2dcf89aa1d47cafc56e487e14e061bacf48ad720
noonbbara <ysys1226@naver.com>
2025-10-20 01:13:18 +0900

    feat(Application): 커스텀 구분자 계산 기능 추가

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -5,25 +5,47 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
 
         System.out.println("덧셈할 문자열을 입력해 주세요.");
         String input = Console.readLine();
         int result = calculate(input);
 
-        System.out.println(result);
+        System.out.println("결과 : " + result);
     }
 
     public static int calculate(String input) {
         if (input == null || input.isEmpty()) throw new IllegalArgumentException();
+        input = input.replace("\\n", "\n");
 
-        String delimiter = ",|:";
+        String delimiter;
+        int sum = 0;
+
+        /**
+         * 커스텀 구분자
+         */
+        if(input.startsWith("//")){
+            delimiter = String.valueOf(input.charAt(2)); // 2번 인덱스에 위치한 문자를 구분자로 사용
+
+            String[] numbers = input.substring(input.indexOf("\n") + 1).split(delimiter);
+            int nn = input.indexOf("\n") + 1;
+            for (String n : numbers) {
+                int num = Integer.parseInt(n);
+                if (num <= 0) throw new IllegalArgumentException();
+                sum += num;
+            }
+
+            return sum;
+        }
+
+        /**
+         * 기본 구분자
+         */
+        delimiter = ",|:";
 
         String[] numbers = input.split(delimiter);
-        int sum = 0;
         for (String n : numbers) {
             int num = Integer.parseInt(n);
-            if (num < 0) throw new IllegalArgumentException();
+            if (num <= 0) throw new IllegalArgumentException();
             sum += num;
         }
 
